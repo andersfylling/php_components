54c5618b86add2f0c6202f3bc2ea843ea3e45785
Anders Øen Fylling <anders@nordic.email>
2016-05-21 05:56:43 +0200

    Added starting version of register..

--- Authenticate/Authenticate.php
+++ Authenticate/Authenticate.php
@@ -41,6 +41,47 @@ class Authenticate
     }
 
     /**
+     * Retrieves the values from the POST. Only checks
+     *  values of keys specified in the parameter.
+     *
+     * @param array $keys
+     * @param string $method
+     * @param string $fieldname
+     * @param string $eventName
+     * @return string or exit() on event firing.
+     */
+    private function getRequestBody
+    (
+        array   $keys,
+        array   &$method, // might be LOADS of data here so make it a reference not a copy!
+        string  $fieldname,
+        string  $eventName = 'emptyField'
+    )
+    {
+        /**
+         * Loop through set keys, set $len to -1 if no matching keys were found.
+         */
+        $len = sizeof($keys);
+        while (--$len !== -1 && isset( $method[$keys[$len]] ) === false);
+
+        if ($len === -1 && empty($keys) === false)
+        {
+            $res =
+                'Could not find any matching keys for '
+                . $fieldname
+                . '. Cannot login with missing credential values!';
+
+            $this->triggerEvent($eventName, [403, $res]);
+
+            return ''; //this will never call if 'emptyField'-event is set.
+        }
+        else
+        {
+            return $method[$keys[$len]];
+        }
+    }
+
+    /**
      * Log in a user. The user object is only filled with info on success.
      *
      * @event error     emptyField      One of the form fields are empty.
@@ -64,39 +105,6 @@ class Authenticate
         array       $keys_csrf          = ['csrf',      'csrftoken',    'token'     ]
     )
     {
-        /**
-         * Retrieves the values from the POST. Only checks 
-         *  values of keys specified in the parameter.
-         *
-         * @param array $keys
-         * @return string or exit() on event trigger.
-         */
-        $getValue = function ($keys, $field)
-        {
-            /**
-             * Loop through set keys, set $len to -1 if no matching keys were found.
-             */
-            $len = sizeof($keys);
-            while (--$len !== -1 && isset( $_POST[$keys[$len]] ) === false);
-
-            if ($len === -1 && empty($keys) === false)
-            {
-                $res =
-                    'Could not find any matching keys for '
-                    . $field
-                    . '. Cannot login with missing credential values!';
-
-                $this->triggerEvent('emptyField', [403, $res]);
-
-                return ''; //this will never call if 'emptyField'-event is set.
-            }
-            else
-            {
-                return $_POST[$keys[$len]];
-            }
-        };
-
-
         /**
          * Make sure it's an POST request!
          */
@@ -126,9 +134,8 @@ class Authenticate
          *
          * Get csrf as csrf | csrftoken | token
          */
-        if ($this->tokens['csrf'] !== $getValue($keys_csrf, 'csrf'))
+        if ($this->tokens['csrf'] !== $this->getRequestBody($keys_csrf, $_POST, 'csrf'))
         {
-            var_dump($getValue($keys_csrf, 'csrf'));
             $this->triggerEvent('error', [403, 'Wrong csrf token.']);
         }
 
@@ -136,8 +143,8 @@ class Authenticate
          * Get username as username | email | login,
          *  password as password | pwd | logintoken,
          */
-        $this->credentials['username']  = $getValue($keys_username, 'username');
-        $this->credentials['password']  = $getValue($keys_password, 'password');
+        $this->credentials['username']  = $this->getRequestBody($keys_username, $_POST, 'username');
+        $this->credentials['password']  = $this->getRequestBody($keys_password, $_POST, 'password');
 
         /**
          * Do a callback to let the user find search results from
@@ -180,4 +187,80 @@ class Authenticate
             $this->triggerEvent('completed', $response);
         });
     } // login method ends here.
+
+    /**
+     * Register a new user. The user object is only filled with info on success.
+     *
+     * TODO: create an array to allow for more input types, phones, country, etc.
+     *
+     * @event error     emptyField      One of the form fields are empty.
+     * @event error     emptyFields     POST was empty.
+     * @event error     error           Something went wrong.
+     * @event success   match           Successfully matched credentials!
+     * @event success   noMatch         Username or password was incorrect.
+     * @event success   completed       Last event to be fired, either login success or failure due to typo.
+     * @event all       *               Listen for any event. Only called if not the event called has been added.
+     *
+     * @param array     $keys_username  username keys that can be found in the POST array. TODO: merge array
+     * @param array     $keys_password  password keys that can be found in the POST array. TODO: merge array
+     * @param array     $keys_csrf      csrf keys that can be found in the POST array.
+     */
+    public function register
+    (
+        array       $keys_username      = ['username',  'email',        'login'     ],
+        array       $keys_password      = ['password',  'pwd',          'logintoken'],
+        array       $keys_csrf          = ['csrf',      'csrftoken',    'token'     ]
+    )
+    {
+        /**
+         * Make sure it's an POST request!
+         */
+        if ($this->config['ajax'] && $_SERVER['REQUEST_METHOD'] !== 'POST')
+        {
+            $this->triggerEvent('error', [403, 'Must be a post request']);
+        }
+
+        /**
+         * If post is empty don't waste time on the loop.
+         *
+         * Don't call an error event as the form might not have been
+         *  submitted on this check.
+         */
+        if ($this->config['ajax'] && empty($_POST) === true)
+        {
+            $this->triggerEvent('error', [403, 'POST cannot be empty!']);
+        }
+        else if (empty($_POST) === true)
+        {
+            return;
+        }
+
+        /**
+         * Before getting the form values, lets verify the csrf token
+         *  if set.
+         *
+         * Get csrf as csrf | csrftoken | token
+         */
+        if ($this->tokens['csrf'] !== $this->getRequestBody($keys_csrf, $_POST, 'csrf'))
+        {
+            $this->triggerEvent('error', [403, 'Wrong csrf token.']);
+        }
+
+        /**
+         * Get username as username | email | login,
+         *  password as password | pwd | logintoken,
+         */
+        $this->credentials['username']  = $this->getRequestBody($keys_username, $_POST, 'username');
+        $this->credentials['password']  = $this->getRequestBody($keys_password, $_POST, 'password');
+
+        /**
+         * After credentials are validated, set it to user.
+         */
+        $this->user = $this->credentials;
+
+        /**
+         * Fire last event "completed".
+         */
+        $this->triggerEvent('completed', [200, 'Successfully registered!']);
+    } // register method ends here.
 }
